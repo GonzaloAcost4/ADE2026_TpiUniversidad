14b49c97b1a0222cd293f47bc3353b44218f9c7f
Maria Fernanda Sosa <129219836+Ferrrchu@users.noreply.github.com>
2026-05-04 07:24:41 -0300

    Agrega transformacion.ipynb y actualiza .env.ex

--- TP2/.env.ex
+++ TP2/.env.ex
@@ -2,4 +2,5 @@ DB_USER = "example_user"
 DB_PASSWORD = "example_root"  # El que configuraste al instalar MySQL
 DB_HOST = "localhost" 
 DB_PORT = "3306"
-DB_DATABASE = "stg_universidad"
+STG_DATABASE = "stg_universidad"
+DWH_DATABASE = "dw_universidad"
