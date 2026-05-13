ab7492431dc2554eb6fe517e75d1c421a13a607d
celeste <cmartinrodich@gmail.com>
2026-05-13 19:46:48 -0300

    carga incremental mediante un script.sh

--- TP2/.env.ex
+++ TP2/.env.ex
@@ -1,6 +1,6 @@
-DB_USER = "root"
-DB_PASSWORD = "root123"  # El que configuraste al instalar MySQL
-DB_HOST = "127.0.0.1"
-DB_PORT = "3306"
-STG_DATABASE = "stg_universidad"
-DWH_DATABASE = "dw_universidad"
+DB_USER="root"
+DB_PASSWORD="root123"  # El que configuraste al instalar MySQL
+DB_HOST="127.0.0.1"
+DB_PORT="3306"
+STG_DATABASE="stg_universidad"
+DWH_DATABASE="dw_universidad"
